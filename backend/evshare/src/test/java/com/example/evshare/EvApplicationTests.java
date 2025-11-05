ab97acdbc09a4ee05dd2a2d0f904b9efe31536e4
Kim Chi <kimchivts27@gmail.com>
2025-11-05 16:11:52 +0700

    init EV_Sharing structure

---
+++ backend/evshare/src/test/java/com/example/evshare/EvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
