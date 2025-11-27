e7792bfe7ad8cb5cac9a534d8779d16cdfc1b0af
Kim Chi <kimchivts27@gmail.com>
2025-11-27 15:09:27 +0700

    Update

--- frontend/src/components/header/UserDropdown.tsx
+++ frontend/src/components/header/UserDropdown.tsx
@@ -35,9 +35,9 @@ export default function UserDropdown() {
         className="flex items-center text-gray-700 dropdown-toggle dark:text-gray-400"
       >
         <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-          <img src="/images/user/owner.jpg" alt="User" />
+          <img src="http://localhost:8080/uploads/avatar.jpg" alt="User" />
         </span>
-        <span className="block mr-1 font-medium text-theme-sm">{name}</span>
+        <span className="block mr-1 font-medium text-theme-sm"></span>
         <svg
           className={`stroke-gray-500 dark:stroke-gray-400 transition-transform duration-200 ${
             isOpen ? "rotate-180" : ""
@@ -64,10 +64,10 @@ export default function UserDropdown() {
         className="absolute right-0 mt-[17px] flex w-[260px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark"
       >
         <div>
-          <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
+          <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400 text-center">
             {name}
           </span>
-          <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-gray-400">
+          <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-gray-400 text-center">
             {email}
           </span>
         </div>
@@ -80,17 +80,7 @@ export default function UserDropdown() {
               to="/profile"
               className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
             >
-              Edit profile
-            </DropdownItem>
-          </li>
-          <li>
-            <DropdownItem
-              onItemClick={closeDropdown}
-              tag="a"
-              to="/profile"
-              className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
-            >
-              Account settings
+              Hồ sơ
             </DropdownItem>
           </li>
         </ul>
@@ -99,7 +89,7 @@ export default function UserDropdown() {
           onClick={handleSignOut}
           className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300 w-full text-left"
         >
-          Sign out
+          Đăng xuất
         </button>
       </Dropdown>
     </div>
