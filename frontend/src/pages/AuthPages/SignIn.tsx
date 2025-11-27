8ae3ada94fd4fd5485e6d57eba958cf35055bd40
Kim Chi <kimchivts27@gmail.com>
2025-11-27 14:50:35 +0700

    Update SignIn.tsx

--- frontend/src/pages/AuthPages/SignIn.tsx
+++ frontend/src/pages/AuthPages/SignIn.tsx
@@ -1,17 +1,14 @@
 import PageMeta from "../../components/common/PageMeta";
-import AuthLayout from "./AuthPageLayout";
 import SignInForm from "../../components/auth/SignInForm";
 
 export default function SignIn() {
   return (
     <>
       <PageMeta
-        title="React.js SignIn Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js SignIn Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="EVCS"
+        description="Hệ thống đồng sở hữu và chia sẻ chi phí xe điện"
       />
-      <AuthLayout>
         <SignInForm />
-      </AuthLayout>
     </>
   );
 }
